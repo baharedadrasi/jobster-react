e4c40caa96604afce22c3047d2eae4b789fb306b
bahare <baharedadrasi@gmail.com>
2022-07-26 20:27:51 +0200

    added logo, error & landing components & routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Landing, Dashboard, Register, Error, PrivateRoute } from './pages';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 function App() {
-  return <h1>jobster</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+        <Route path="landing" element={<Landing />} />
+        <Route path="register" element={<Register />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Landing from './Landing';
+import Dashboard from './Dashboard';
+import Register from './Register';
+import Error from './Error';
+import PrivateRoute from './PrivateRoute';
+
+export { Landing, Dashboard, Register, Error, PrivateRoute };
